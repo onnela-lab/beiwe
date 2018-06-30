222b92c2dd9c79fd8cfed51b6e41f94f0043fb95
JP Onnela <jponnela@hsph.harvard.edu>
2018-06-30 10:16:37 -0400

    added Beiwe data downloading script

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,71 @@
 import glob
 import os
+import datetime
+
+try:
+    import mano
+    import logging
+    import mano.sync as msync
+except:
+    print("Failed importing nano. This needs to be run in a pipenv environment to work.")
+
+
+def download_beiwe_data(study_no=2, output_folder= "/tmp/beiwe-data", time_end=None, time_start=None):
+    """
+    Download Beiwe data to a local directory. If time_start is not specified, extraction will
+    proceed one week from time_end. If time_end is not specified, today's date is use as default.
+
+    Args:
+        study_no (int): Study number 1-3
+        output_folder (str): Downloaded data will be stored here
+        time_end (str): End date of extraction in the format YYYY-MM-DD
+        time_start (str): Start date of extraction in the format YYYY-MM-DD
+
+    Returns:
+
+    """
+
+    # specify keyring and data streams
+    Keyring = mano.keyring("beiwe.onnela")
+    data_streams = ["power_state", "survey_answers"]
+    logging.basicConfig(level=logging.INFO)
+
+    # choose study
+    studies = {1: ("UCSD_Little_UNI_HIV_Pos", "5a3a856203d3c42e31329970"), \
+               2: ("UCSD_Little_UNI_HIV_Neg", "5a3a85db03d3c42e3132998a"), \
+               3: ("UCSD_Little_RDS Study", "5a3a862503d3c42e3217bb90")}
+    (study_name, study_id) = studies[study_no]
+    print("  Processing study %s (study ID %s)." % (study_name, study_id))
+
+    # specify time_end
+    if time_end is None:
+        time_end = datetime.datetime.today().strftime("%Y-%m-%d") + "T00:00:00"
+    else:
+        if len(time_end) == 10:
+            time_end = time_end + "T00:00:00"
+
+    # specify time_start
+    if time_start is None:
+        end_time = datetime.datetime.strptime(time_end, "%Y-%m-%dT%H:%M:%S") 
+        start_time = end_time - datetime.timedelta(days=7)
+        time_start = start_time.strftime("%Y-%m-%dT%H:%M:%S")
+    else:
+        if len(time_start) == 10:
+            time_start = time_start + "T00:00:00"
+    print("  Extracting data from %s to %s to %s." % (time_start, time_end, output_folder))
+
+    # loop over all user IDs in the system for the study and download data
+    for user_id in mano.users(Keyring, study_id):
+        print("  Downloading data for user %s." % user_id)
+        zf = msync.download(Keyring, study_id, user_id, data_streams=data_streams, time_start=time_start, time_end=time_end)
+        zf.extractall(output_folder)
+
+    # identify users for whom we have any data (folder exists)
+    active_users = os.listdir(output_folder)
+    active_users.remove("registry")
+    print("\n  Found data for the following users:")
+    for user_id in active_users:
+        print(" ", user_id)
 
 
 def check_file_size(data_dir, dates, subjects, surveys, data_stream_indices):
